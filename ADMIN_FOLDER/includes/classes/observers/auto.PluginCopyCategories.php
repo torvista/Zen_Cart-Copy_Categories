75934850030bff962d11bf1ee00e7caedcd23185
torvista <4391026+torvista@users.noreply.github.com>
2022-11-30 23:32:11 +0100

    alpha

--- ADMIN_FOLDER/includes/classes/observers/auto.PluginCopyCategories.php
+++ ADMIN_FOLDER/includes/classes/observers/auto.PluginCopyCategories.php
@@ -15,8 +15,6 @@ if (!defined('IS_ADMIN_FLAG') || IS_ADMIN_FLAG !== true) {
  */
 class zcObserverPluginCopyCategories extends base
 {
-    var bool $debug = false;
-
     public function __construct()
     {
         // Load the language definition file for the current language
@@ -63,59 +61,75 @@ class zcObserverPluginCopyCategories extends base
         }
 
         //process the copy confirm
-        if ($p1 === 'copy_category_confirm') {
+        if ($p1 === 'copy_category_confirm' && isset($_POST['copy_from_category_id'], $_POST['copy_to_category_id'])) {
             $p2 = false;
+            $copy_from_category_id = (int)$_POST['copy_from_category_id'];
+            $new_parent_id = (int)$_POST['copy_to_category_id'];
+            $categories_with_products = [];
 
-            if (isset($_POST['categories_id'], $_POST['copy_to_category_id'])) {
-                $categories_id = (int)$_POST['categories_id'];
-                $new_parent_id = (int)$_POST['copy_to_category_id'];
-                $categories_with_products = [];
-
-                $products_in_categories = zen_get_categories_products_list($new_parent_id, true);
-                if (count($products_in_categories) > 0) {
-                    foreach ($products_in_categories as $key => $value) {
-                        $categories_with_products[] = substr($value, strrpos($value, "_"));
-                    }
-                    //should be only one value
-                    $categories_with_products = array_unique($categories_with_products);
+            $products_in_categories = zen_get_categories_products_list($new_parent_id, true, false);
+            if (count($products_in_categories) > 0) {
+                mv_printVar($products_in_categories);
+                foreach ($products_in_categories as $key => $value) {
+                    echo '$value='.$value.'<br>';
+                    $categories_with_products[] = strrpos($value, "_") === false ? $value : substr($value, strrpos($value, "_"));
                 }
-                //abort if there are products in the destination category
-                if (in_array($new_parent_id, $categories_with_products)) {
-                    $messageStack->add_session(sprintf(TEXT_COPY_CATEGORY_PRODUCTS_IN_TARGET, zen_get_category_name($new_parent_id), $new_parent_id), 'error');
-                    zen_redirect(zen_href_link(FILENAME_CATEGORY_PRODUCT_LISTING, 'cPath=' . $cPath));
-                } else {
-                    $this->_copy_categories_tree($categories_id, $new_parent_id);
-                    zen_redirect(zen_href_link(FILENAME_CATEGORY_PRODUCT_LISTING, 'cPath=' . (int)$_POST['copy_to_category_id']));
-                }
+                //should be only one value
+                $categories_with_products = array_unique($categories_with_products);
+            }
+            //abort if there are products in the destination category
+            if (in_array($new_parent_id, $categories_with_products)) {
+                $messageStack->add_session(sprintf(TEXT_COPY_CATEGORIES_PRODUCTS_IN_TARGET, zen_get_category_name($new_parent_id), $new_parent_id), 'error');
+                zen_redirect(zen_href_link(FILENAME_CATEGORY_PRODUCT_LISTING, 'cPath=' . $cPath));
+            } else {
+                $this->_copy_categories_tree($copy_from_category_id, $new_parent_id);
+                zen_redirect(zen_href_link(FILENAME_CATEGORY_PRODUCT_LISTING, 'cPath=' . (int)$_POST['copy_to_category_id']));
             }
         }
     }
 
-//handle the copy-category action t display the infobox
+//handle the copy-category action to display the infobox
 //$zco_notifier->notify('NOTIFY_ADMIN_PROD_LISTING_DEFAULT_INFOBOX', $action, $heading, $contents);
     public function notify_admin_prod_listing_default_infobox(&$class, $eventID, $p1, &$p2, &$p3)
     {
-        global $cInfo, $cPath, $current_category_id;
+        global $cInfo, $cPath, $current_category_id, $languages;
         if (!empty($p2) && !empty($p3)) {
             return;
         }
         if ($p1 === 'copy_category') {
-            $heading[] = ['text' => '<b>' . sprintf(TEXT_INFO_HEADING_COPY_CATEGORY, $cInfo->categories_name, $cInfo->categories_id) . '</b>'];
-            $contents = ['form' => zen_draw_form('categories', FILENAME_CATEGORY_PRODUCT_LISTING, 'action=copy_category_confirm&cPath=' . $cPath, 'post', 'class="form-horizontal"') . zen_draw_hidden_field('categories_id', $cInfo->categories_id)];
+            $heading[] = ['text' => '<b>' . sprintf(TEXT_COPY_CATEGORIES_INFO_HEADING, $cInfo->categories_name, $cInfo->categories_id) . '</b>'];
+            $contents = [
+                'form' => zen_draw_form('categories', FILENAME_CATEGORY_PRODUCT_LISTING, 'action=copy_category_confirm&cPath=' . $cPath, 'post', 'class="form-horizontal"') .
+                    zen_draw_hidden_field('copy_from_category_id', $cInfo->categories_id)
+            ];
 
             $contents[] = ['text' => TEXT_COPY_CATEGORIES_INTRO];
+
+            foreach ($languages as $language) {
+                //[0] => Array
+                //        (
+                //            [id] => 1
+                //            [name] => English
+                //            [code] => en
+                //            [image] => icon.gif
+                //            [directory] => english
+                //        )
+                $category_name_lang = zen_get_category_name($cInfo->categories_id, $language['id']);
+                $contents[] = [
+                    'text' => TEXT_COPY_CATEGORIES_NAME .
+                        (count($languages) > 1 ? ' (' . $language['directory'] . ')' : '') . '<br>' .
+                        zen_draw_input_field('copied_category_names[' . $language['id'] . ']', $category_name_lang, 'placeholder="' . $category_name_lang . '" size="31" maxlength="30" class=form-control')
+                ];
+            }
+            $contents[] = ['text' => '<label>' . zen_draw_checkbox_field('copy_metatags', 'yes', true) . ' ' . TEXT_COPY_CATEGORIES_METATAGS . '</label>'];
+            $contents[] = ['text' => '<label>' . zen_draw_checkbox_field('enable_copied_categories', 'yes', true) . ' ' . TEXT_COPY_CATEGORIES_CATEGORY_ENABLED . '</label>'];
             $contents[] = [
-                'text' => TEXT_COPY_CATEGORIES_NAME . '<br>' . zen_draw_input_field('copied_category_name', $cInfo->categories_name, 'placeholder="' . $cInfo->categories_name . '" size="31" maxlength="30" class=form-control')
+                'text' => '<b>' . TEXT_COPY_CATEGORIES_COPY_PRODUCTS . '</b>'
+                    . '<div class="radio"><label>' . zen_draw_radio_field('copy_products', 'copy_products_no', true) . TEXT_COPY_CATEGORIES_COPY_PRODUCTS_NO . '</label></div>'
+                    . '<div class="radio"><label>' . zen_draw_radio_field('copy_products', 'copy_products_linked') . TEXT_COPY_CATEGORIES_COPY_PRODUCTS_LINKED . '</label></div>'
             ];
-            $contents[] = ['text' => '<label>' . zen_draw_checkbox_field('copy_metatags', 'on', true) . ' ' . TEXT_COPY_CATEGORY_METATAGS . '</label>'];
-            $contents[] = [
-                'text' => '<b>' . TEXT_COPY_CATEGORY_COPY_PRODUCTS . '</b>'
-                    . '<div class="radio"><label>' . zen_draw_radio_field('copy_products', 'copy_products_no', true) . TEXT_COPY_CATEGORY_COPY_PRODUCTS_NO . '</label></div>'
-                    . '<div class="radio"><label>' . zen_draw_radio_field('copy_products', 'copy_products_linked') . TEXT_COPY_CATEGORY_COPY_PRODUCTS_LINKED . '</label></div>'
-                //. '<div class="radio"><label>' . zen_draw_radio_field('copy_products', 'copy_products_duplicate') . TEXT_COPY_CATEGORY_COPY_PRODUCTS_DUPLICATE . '</label></div>'
-            ];
-            $contents[] = ['text' => sprintf(TEXT_COPY_CATEGORY, $cInfo->categories_name, $cInfo->categories_id)];
-            $test = zen_get_category_tree();
+            $contents[] = ['text' => sprintf(TEXT_COPY_CATEGORIES, $cInfo->categories_name, $cInfo->categories_id)];
+
             $contents[] = ['text' => zen_draw_pull_down_menu('copy_to_category_id', zen_get_category_tree(), $current_category_id, 'class="form-control"')];
             $contents[] = [
                 'align' => 'center',
@@ -128,68 +142,66 @@ class zcObserverPluginCopyCategories extends base
 
     private function _copy_categories_tree($source_category_id, $target_parent_id): void
     {
-        global $cPath, $db, $messageStack;
-
-        if ($this->debug) {
-            echo 'fn _copy_categories_tree START<br>';
-        }
+        global $db, $languages, $messageStack;
 
         //insert new category record
-        $db->Execute(
-            'INSERT INTO ' . TABLE_CATEGORIES . '
-            (categories_image, parent_id, sort_order, date_added, last_modified, categories_status)
-                  SELECT categories_image, ' . $target_parent_id . ' AS parent_id, sort_order, NOW() AS date_added, NOW() AS last_modified, categories_status
-                  FROM ' . TABLE_CATEGORIES . ' WHERE categories_id = ' . $source_category_id
-        );
+        $sql = 'INSERT INTO ' . TABLE_CATEGORIES . ' (categories_image, parent_id, sort_order, date_added, last_modified, categories_status)
+                  SELECT categories_image, :target_parent_id: AS parent_id, sort_order, NOW() AS date_added, NOW() AS last_modified, :categories_status: AS categories_status
+                  FROM ' . TABLE_CATEGORIES . ' WHERE categories_id = :source_category_id:';
+        $sql = $db->bindVars($sql, ':target_parent_id:', $target_parent_id, 'integer');
+        $sql = $db->bindVars($sql, ':source_category_id:', $source_category_id, 'integer');
+        $sql = $db->bindVars($sql, ':categories_status:', (isset($_POST['enable_copied_categories']) && $_POST['enable_copied_categories'] === 'yes' ? 1 : 0), 'integer');
+        //echo '$sql=' . $sql . '<br>';die;
+        $db->Execute($sql);
 
-        //get the category_id of the new/target category record
+        //get the category_id of the new/target category
         $target_category_id = zen_db_insert_id();
 
-        //copy category details from source to target
-        if (isset($_POST['copied_category_name']) && (int)$source_category_id === (int)$_POST['categories_id']) {
-            $category_name = '"' . zen_db_prepare_input($_POST['copied_category_name']) . '"';
-        } else {
-            $category_name = 'categories_name';
-        }
-
-        $db->Execute(
-            'INSERT INTO ' . TABLE_CATEGORIES_DESCRIPTION . ' (categories_id, language_id, categories_name, categories_description)
-                  SELECT ' . $target_category_id . ' AS categories_id, language_id, ' . $category_name . ', categories_description
-                FROM ' . TABLE_CATEGORIES_DESCRIPTION . ' WHERE categories_id = ' . $source_category_id
-        );
-
-        if (isset($_POST['copy_metatags']) && $_POST['copy_metatags'] === 'on') {
-            $db->Execute(
-                'INSERT INTO ' . TABLE_METATAGS_CATEGORIES_DESCRIPTION . ' (categories_id, language_id, metatags_title, metatags_keywords, metatags_description)
-                  SELECT ' . $target_category_id . ' AS categories_id, language_id, metatags_title, metatags_keywords, metatags_description
-                FROM ' . TABLE_METATAGS_CATEGORIES_DESCRIPTION . ' WHERE categories_id = ' . $source_category_id
-            );
-        }
-
-        $messageStack->add_session(sprintf(TEXT_COPY_CATEGORY_TO_CATEGORY, zen_get_category_name($source_category_id), $source_category_id, zen_get_category_name($target_parent_id), $target_parent_id), 'success');
-
-        if ($this->debug) {
-            echo 'fn _copy ' . __LINE__ . ':$source_category_id=' . $source_category_id . ' | $target_parent_id=' . $target_parent_id . ' | $target_category_id=' . $target_category_id . '<br>';
-        }
-
-        if ($_POST['copy_products'] === 'copy_products_linked' || $_POST['copy_products'] === 'copy_products_duplicate') {
-            if ($this->debug) {
-                echo 'fn _copy ' . __LINE__ . ':copy_products<br>';
+        //on first run, the new category name comes from the POST. On the subsequent/recursive runs, new category name must come from the source
+        //language loop
+        foreach ($languages as $key => $language) {
+            if (isset($_POST['copied_category_names'])) {
+                $sql = 'INSERT INTO ' . TABLE_CATEGORIES_DESCRIPTION . ' (categories_id, language_id, categories_name, categories_description)
+                  SELECT :target_category_id: AS categories_id, language_id, :category_name: AS categories_name, categories_description
+                  FROM ' . TABLE_CATEGORIES_DESCRIPTION . ' WHERE categories_id = :source_category_id: AND language_id = :language_id:';
+                $sql = $db->bindVars($sql, ':category_name:', $_POST['copied_category_names'][$language['id']], 'string');
+            } else {
+                $sql = 'INSERT INTO ' . TABLE_CATEGORIES_DESCRIPTION . ' (categories_id, language_id, categories_name, categories_description)
+                  SELECT :target_category_id: AS categories_id, language_id, categories_name, categories_description
+                  FROM ' . TABLE_CATEGORIES_DESCRIPTION . ' WHERE categories_id = :source_category_id: AND language_id = :language_id:';
             }
+            $sql = $db->bindVars($sql, ':target_category_id:', $target_category_id, 'integer');
+            $sql = $db->bindVars($sql, ':source_category_id:', $source_category_id, 'integer');
+            $sql = $db->bindVars($sql, ':language_id:', $language['id'], 'integer');
+            // echo '$sql=' . $sql . '<br>';die;
+            $db->Execute($sql);
 
+            if (isset($_POST['copy_metatags']) && $_POST['copy_metatags'] === 'yes') {
+                $sql = 'INSERT INTO ' . TABLE_METATAGS_CATEGORIES_DESCRIPTION . ' (categories_id, language_id, metatags_title, metatags_keywords, metatags_description)
+                  SELECT :target_category_id: AS categories_id, :language_id: AS language_id, metatags_title, metatags_keywords, metatags_description
+                FROM ' . TABLE_METATAGS_CATEGORIES_DESCRIPTION . ' WHERE categories_id = :source_category_id: AND language_id = :language_id:';
+                $sql = $db->bindVars($sql, ':target_category_id:', $target_category_id, 'integer');
+                $sql = $db->bindVars($sql, ':language_id:', $key, 'integer');
+                $sql = $db->bindVars($sql, ':source_category_id:', $source_category_id, 'integer');
+                // echo '$sql=' . $sql . '<br>';die;
+                $db->Execute($sql);
+            }
+        }
+        unset($_POST['copied_category_names']);
+
+        $messageStack->add_session(sprintf(TEXT_COPY_CATEGORIES_TO_CATEGORY, zen_get_category_name($source_category_id), $source_category_id, zen_get_category_name($target_parent_id), $target_parent_id), 'success');
+
+        if ($_POST['copy_products'] === 'copy_products_linked' || $_POST['copy_products'] === 'copy_products_duplicate') {//todo duplicate, maybe
             $products_to_copy = zen_get_categories_products_list($source_category_id, true, false);
             //function creates $categories_products_id_list as a global. Need to unset it here or products in each category are appended to array
             unset ($GLOBALS['categories_products_id_list']);
 
-            if ($this->debug) {
-                echo 'fn _copy ' . __LINE__;
-            }
             if ($_POST['copy_products'] === 'copy_products_linked') {
                 foreach ($products_to_copy as $key => $source_category) {
                     zen_link_product_to_category($key, $target_category_id);
-                    $messageStack->add_session(sprintf(TEXT_COPY_CATEGORY_PRODUCTS_LINKED, zen_get_products_name($key), $key, zen_get_category_name($source_category), $source_category, zen_get_category_name($target_category_id), $target_category_id), 'success');
+                    $messageStack->add_session(sprintf(TEXT_COPY_CATEGORIES_PRODUCTS_LINKED, zen_get_products_name($key), $key, zen_get_category_name($source_category), $source_category, zen_get_category_name($target_category_id), $target_category_id), 'success');
                 }
-            } else {//todo duplicate products:need to use copy_product_confirm in a loop
+            } else {//todo: duplicate products need to use copy_product_confirm in a loop
                 echo '';//prevent inspection warning for empty statement
             }
         }
@@ -200,9 +212,6 @@ class zcObserverPluginCopyCategories extends base
         }
         foreach ($child_ids as $child) {
             $this->_copy_categories_tree($child, $target_category_id);
-        }
-        if ($this->debug) {
-            echo 'fn _copy_categories_tree END<br>';
         }
     }
 }
--- ADMIN_FOLDER/includes/languages/english/plugin_copy_categories.php
+++ ADMIN_FOLDER/includes/languages/english/plugin_copy_categories.php
@@ -6,14 +6,16 @@ declare(strict_types=1);
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version torvista 2022 Nov 29
  */
-define('TEXT_INFO_HEADING_COPY_CATEGORY', 'Copy category "%1$s" (#%2$u)');
+
+define('TEXT_COPY_CATEGORIES', 'Copy Category: "<b>%1$s</b>" (#%2$u) to:');
+define('TEXT_COPY_CATEGORIES_CATEGORY_ENABLED', 'Enable the copied category/categories?');
+define('TEXT_COPY_CATEGORIES_COPY_PRODUCTS', 'Copy products too?');
+define('TEXT_COPY_CATEGORIES_COPY_PRODUCTS_LINKED', 'Copy products as <b>linked</b>');
+define('TEXT_COPY_CATEGORIES_COPY_PRODUCTS_NO', 'Do <b>not</b> copy products, only the category structure.');
+define('TEXT_COPY_CATEGORIES_INFO_HEADING', 'Copy category "%1$s" (#%2$u)');
 define('TEXT_COPY_CATEGORIES_INTRO', 'Duplicate this category and it\'s subcategories in another parent category.');
-define('TEXT_COPY_CATEGORIES_NAME', 'New category name (optional):');
-define('TEXT_COPY_CATEGORY', 'Copy category "<b>%1$s</b>" (#%2$u) to:');
-define('TEXT_COPY_CATEGORY_METATAGS', 'Copy category metatags');
-define('TEXT_COPY_CATEGORY_COPY_PRODUCTS', 'Copy products too?');
-define('TEXT_COPY_CATEGORY_COPY_PRODUCTS_NO', 'Do <b>not</b> copy products, only the category structure.');
-define('TEXT_COPY_CATEGORY_COPY_PRODUCTS_LINKED', 'Copy products as <b>linked</b>');
-define('TEXT_COPY_CATEGORY_TO_CATEGORY', '<b>Category:</b> "%1$s" (#%2$u) copied to category "%3$s" (#%4$u)');
-define('TEXT_COPY_CATEGORY_PRODUCTS_LINKED', 'Product: "%1$s</b>" (#%2$u) in source category "%3$s" (#%4$u), linked to target category "%5$s" (#%6$u)');
-define('TEXT_COPY_CATEGORY_PRODUCTS_IN_TARGET', 'ERROR: Copy Category aborted, target category "%1$s" (#%2$u) contains product(s).');
+define('TEXT_COPY_CATEGORIES_METATAGS', 'Copy category metatags');
+define('TEXT_COPY_CATEGORIES_NAME', 'New category name');
+define('TEXT_COPY_CATEGORIES_PRODUCTS_IN_TARGET', 'ERROR: Copy Category aborted, target category "%1$s" (#%2$u) contains product(s).');
+define('TEXT_COPY_CATEGORIES_PRODUCTS_LINKED', 'Product: "%1$s</b>" (#%2$u) in source category "%3$s" (#%4$u), linked to target category "%5$s" (#%6$u)');
+define('TEXT_COPY_CATEGORIES_TO_CATEGORY', '<b>Category:</b> "%1$s" (#%2$u) copied to category "%3$s" (#%4$u)');
